83564a03a38069e0be7f7008faaa720cc507c83e
Carsten Gips <carsten.gips@fh-bielefeld.de>
2016-05-02 16:40:44 +0200

    Vorgaben zu B08

---
+++ b08/refactoring/Rental.java
@@ -0,0 +1,21 @@
+package b08.refactoring;
+
+public class Rental {
+
+    public Movie movie;
+    public int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+}
